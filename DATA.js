8f52561375df5280beaeefb68d8b5b04654e5494
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 17:11:49 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "da01cf0f-4c65-4ee9-b07a-d3c9b87ebed4",
+    "home_feed": [
+        {
+            "id": "bf195b0c-86a5-4e93-89b4-b62a57d0df63",
+            "artist_name": "find",
+            "home_carousel": [
+                {
+                    "id": "8d83855c-36d0-473c-a239-6a56d8fb720f",
+                    "art_photo": "https://images.unsplash.com/photo-1575980297904-f3f1170539d7?w=1080"
+                },
+                {
+                    "id": "e0a205d3-1b5a-4daf-9c67-2b706d8c8552",
+                    "art_photo": "https://images.unsplash.com/photo-1579389082366-6a268f7c20e0?w=1080"
+                },
+                {
+                    "id": "3f872d0b-0870-45f8-8c4a-3883900b712b",
+                    "art_photo": "https://images.unsplash.com/photo-1602039269866-5d04a668f6a2?w=1080"
+                },
+                {
+                    "id": "2cb25fe4-d77f-42b6-94c9-7ad9ec9b9a4a",
+                    "art_photo": "https://images.unsplash.com/photo-1571261354542-7dd5fde6d461?w=1080"
+                },
+                {
+                    "id": "c27fb20e-9635-4045-82f7-6bc6e34773a4",
+                    "art_photo": "https://images.unsplash.com/photo-1614540648095-3b1a0bf29570?w=1080"
+                }
+            ],
+            "art_caption": "seat",
+            "art_description": "Process start my color degree lot large. Recent remember teach name model which."
+        },
+        {
+            "id": "51c6119c-17d6-44e7-a775-6bf46dbef9df",
+            "artist_name": "step",
+            "home_carousel": [
+                {
+                    "id": "5f8c8a0b-4c08-4801-b569-1cead2c50250",
+                    "art_photo": "https://images.unsplash.com/photo-1496101813905-2dde51604269?w=1080"
+                },
+                {
+                    "id": "478e75e2-83ce-4a68-833f-f728868393de",
+                    "art_photo": "https://images.unsplash.com/photo-1496302213534-54a1c565ffbf?w=1080"
+                },
+                {
+                    "id": "abe917ad-196f-4706-ae71-7a7b6f6ab17b",
+                    "art_photo": "https://images.unsplash.com/photo-1513118172236-00b7cc57e1fa?w=1080"
+                },
+                {
+                    "id": "6464d2ec-424b-434a-80bb-606b07520039",
+                    "art_photo": "https://images.unsplash.com/source-404?w=1080"
+                },
+                {
+                    "id": "8e206c06-4a6a-4043-8c01-b95ef3e166ed",
+                    "art_photo": "https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?w=1080"
+                }
+            ],
+            "art_caption": "soldier",
+            "art_description": "Consumer image of few his simply."
+        },
+        {
+            "id": "90d9bb05-2e4e-4548-bbbe-3078b5c72f31",
+            "artist_name": "pull",
+            "home_carousel": [
+                {
+                    "id": "86ee29a0-94a5-4dd7-a200-024b376ab353",
+                    "art_photo": "https://images.unsplash.com/photo-1583248881102-32809a3f6130?w=1080"
+                },
+                {
+                    "id": "a4359397-0d12-4836-9936-f5ed9323a740",
+                    "art_photo": "https://images.unsplash.com/source-404?w=1080"
+                },
+                {
+                    "id": "53935bde-d2dc-4f8c-973f-ee513380d0ba",
+                    "art_photo": "https://images.unsplash.com/photo-1460518451285-97b6aa326961?w=1080"
+                },
+                {
+                    "id": "340eda57-c548-4a6a-baea-1901499e45ed",
+                    "art_photo": "https://images.unsplash.com/photo-1614594642171-902bc291ff3f?w=1080"
+                },
+                {
+                    "id": "cd8133b3-8bf8-44ab-a24a-1e8481e1e4f3",
+                    "art_photo": "https://images.unsplash.com/photo-1573522259101-c18369ead8cc?w=1080"
+                }
+            ],
+            "art_caption": "health",
+            "art_description": "Sing important beautiful somebody along culture season. Real science fill. Capital party white treat political box."
+        },
+        {
+            "id": "4f3c7643-7ef8-460c-a07a-51711e7ac0d8",
+            "artist_name": "never",
+            "home_carousel": [
+                {
+                    "id": "c828be92-2dee-4b21-9684-8ab329f214d8",
+                    "art_photo": "https://images.unsplash.com/photo-1514533450685-4493e01d1fdc?w=1080"
+                },
+                {
+                    "id": "e9bfc291-0af9-4fd8-826b-92265cd589cb",
+                    "art_photo": "https://images.unsplash.com/photo-1628149183763-3c145ee4aa27?w=1080"
+                },
+                {
+                    "id": "fa551d82-426c-4338-af1a-0da45b119e4f",
+                    "art_photo": "https://images.unsplash.com/photo-1520499737119-4e70f75b339f?w=1080"
+                },
+                {
+                    "id": "efe9c80f-a45a-4312-b284-da37b8dfbbda",
+                    "art_photo": "https://images.unsplash.com/photo-1625806335823-d967b0099d2b?w=1080"
+                },
+                {
+                    "id": "9a35d8a6-eea0-4e2a-be00-eab9a6b190d7",
+                    "art_photo": "https://images.unsplash.com/photo-1559333203-e5222606a9db?w=1080"
+                }
+            ],
+            "art_caption": "station",
+            "art_description": "Parent image difference. Second each see foreign will authority meet happen."
+        },
+        {
+            "id": "ae1dd834-99e4-4910-b9dd-92c7ae93ae10",
+            "artist_name": "change",
+            "home_carousel": [
+                {
+                    "id": "b0a3cd66-e1c2-4660-a36f-ea595e37e9cc",
+                    "art_photo": "https://images.unsplash.com/photo-1556761175-5973dc0f32e7?w=1080"
+                },
+                {
+                    "id": "9b26062b-6698-4ff6-bf47-6137973b30b3",
+                    "art_photo": "https://images.unsplash.com/photo-1625631246362-62c2b65e1894?w=1080"
+                },
+                {
+                    "id": "9d200183-a3c8-440e-85f5-3dd550c14db3",
+                    "art_photo": "https://images.unsplash.com/photo-1604948501466-4e9c339b9c24?w=1080"
+                },
+                {
+                    "id": "61739aa5-2bf4-4d42-9817-ef3f50f91425",
+                    "art_photo": "https://images.unsplash.com/photo-1527018002759-43baaa3ea12a?w=1080"
+                },
+                {
+                    "id": "69ed445a-3c4a-4388-b9a1-25c015e3892a",
+                    "art_photo": "https://images.unsplash.com/photo-1636629198288-8fe85b92110a?w=1080"
+                }
+            ],
+            "art_caption": "leave",
+            "art_description": "Hair close why generation first between. Bed condition interesting billion technology owner smile."
+        }
+    ]
+}
